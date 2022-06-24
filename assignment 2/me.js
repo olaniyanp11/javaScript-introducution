b4578cb3585d6714d580f849fd530e2de66c9af4
olaniyanp11 <107733620+olaniyanp11@users.noreply.github.com>
2022-06-24 00:39:21 -0700

    Add files via upload

---
+++ assignment 2/me.js
@@ -0,0 +1,9 @@
+const name = "<b>olaniyan pelumi</b>";
+const height = "<b>6ft tall</b>";
+const country = "<b>Nigeria</b>";
+let row1 = "<b>my name</b>";
+let row2 = "<b>my height</b>";
+let row3 = "<b>my country</b>";
+document.write(row1 + " : " + name + "</br>");
+document.write(row2 + " : " + height + "</br>");
+document.write(row3 + " : " + country);
